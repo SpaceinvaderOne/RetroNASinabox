f81b1f97f819a2911da8faa7203ca4c7b7d737e3
SpaceinvaderOne <54743094+SpaceinvaderOne@users.noreply.github.com>
2023-02-24 11:30:37 +0000

    add checksum verification and other stuff

--- letsgo.sh
+++ letsgo.sh
@@ -10,7 +10,6 @@ RETRO_SHARE="/mnt/user/retronas"
 XML_FILE="/tmp/retro.xml"
 
 #-----------------------------------------------------------
-#-----------------------------------------------------------
 
 if [ "$container" = "yes" ]; then
 	# Get Variables as they will have been set in Docker as variables and bind mounts
@@ -29,55 +28,62 @@ else
 	vm_name="$vm_name"
 	domains_share="$domains_share"
 	RETRO_SHARE="$RETRO_SHARE"
+	expected_checksum="f80ab102b7ab1fec81cfc3d7b929dbd9"
 fi
 
 download_location="$domains_share/$vm_name"
 
 #-----------------------------------------------------------
 
-download_retronas () {
+download_retronas() {
+    # Expected MD5 checksum for the retronas.zip file
+    expected_checksum="f80ab102b7ab1fec81cfc3d7b929dbd9"
+
     # Create download location if it doesn't exist
     if [ ! -d "$download_location" ]; then
         echo "Download location directory does not exist. Creating it..."
         mkdir -p "$download_location"
     fi
 
-    # Download and decompress the file
+    # Download the file
     for link in "$link1" "$link2" "$link3"; do
         echo "Downloading file from link: $link ..."
         curl -L -c /tmp/cookies "https://drive.google.com/uc?export=download&id=$(echo "$link" | cut -d'/' -f6)" > /dev/null
         confirm=$(awk '/download/ {print $NF}' /tmp/cookies)
         curl -L -b /tmp/cookies "https://drive.google.com/uc?export=download&confirm=$confirm&id=$(echo "$link" | cut -d'/' -f6)" > "$download_location/retronas.zip"
 
-        # Check if the downloaded file is greater than 500MB
-        if [ "$(du -m "$download_location/retronas.zip" | cut -f1)" -ge 500 ]; then
-            echo "Decompressing file..."
-            unzip -o "$download_location/retronas.zip" -d "$download_location"
+        # Verify the checksum of the downloaded file
+        downloaded_checksum=$(md5sum "$download_location/retronas.zip" | cut -d' ' -f1)
+        if [ "$downloaded_checksum" != "$expected_checksum" ]; then
+            echo "Downloaded file has an incorrect checksum. Trying next link..."
+            rm -f "$download_location/retronas.zip"
+            continue
+        fi
 
-            # Check if the file was successfully decompressed
-            if [ $? -eq 0 ]; then
-                # Delete the original file
-                rm -f "$download_location/retronas.zip"
+        # Decompress the file
+        echo "Decompressing file..."
+        unzip -o "$download_location/retronas.zip" -d "$download_location"
 
-				# Check if vdisk1.img file exists and rename it if it does
-				if [ -f "$download_location/vdisk1.img" ]; then
-					old_filename=$(date +"vdisk1-old-%Y-%m-%d-%H-%M.img")
-					echo "Moving existing vdisk1.img file to $old_filename..."
-					mv "$download_location/vdisk1.img" "$download_location/$old_filename"
-				fi
+        # Check if the file was successfully decompressed
+        if [ $? -eq 0 ]; then
+            # Delete the original file
+            rm -f "$download_location/retronas.zip"
 
-        echo "Moving decompressed file to $download_location/vdisk1.img..."
-        mv "$download_location/retronas.img" "$download_location/vdisk1.img"
-        break
-    else
-        echo "Failed to decompress file."
-        exit 1
-    fi
-else
-    echo "Failed to download file from link: $link"
-    rm -f "$download_location/retronas.zip"
-fi
-done
+            # Check if vdisk1.img file exists and rename it if it does
+            if [ -f "$download_location/vdisk1.img" ]; then
+                old_filename=$(date +"vdisk1-old-%Y-%m-%d-%H-%M.img")
+                echo "Moving existing vdisk1.img file to $old_filename..."
+                mv "$download_location/vdisk1.img" "$download_location/$old_filename"
+            fi
+
+            echo "Moving decompressed file to $download_location/vdisk1.img..."
+            mv "$download_location/retronas.img" "$download_location/vdisk1.img"
+            break
+        else
+            echo "Failed to decompress file."
+            exit 1
+        fi
+    done
 }
 
 #-----------------------------------------------------------
@@ -99,10 +105,16 @@ define_retronas() {
 
 	# Replace the name of the virtual machine in the XML file with the specified name
 	sed -i "s#<name>.*<\/name>#<name>$vm_name<\/name>#" "$XML_FILE"
+	
+	# Replace the icon location in the XML file with the specified location
+    sed -i "s#<vmtemplate xmlns=\"unraid\" name=\"RetroNAS\" icon=\"retronas\" os=\"debian\"/>#<vmtemplate xmlns=\"unraid\" name=\"$vm_name\" icon=\"$download_location/RetroNAS_Icon.png\" os=\"debian\"/>#" "$XML_FILE"
+
 
 	# Define the virtual machine using the modified XML file
 	virsh define "$XML_FILE"
 }
+
+#-----------------------------------------------------------
 
 function find_mappings {
 	# Find the host path of the directory mapped to $1
@@ -118,8 +130,24 @@ function download_xml {
 	curl -s -L $url -o $XML_FILE
 }
 
-download_retronas
-define_retronas
+#-----------------------------------------------------------
 
-echo "done"
+function download_icon {
+	local url="https://raw.githubusercontent.com/SpaceinvaderOne/RetroNASinabox/main/RetroNAS_Icon.png"
+	curl -s -L $url -o $download_location
+}
+
+#-----------------------------------------------------------
+
+download_retronas
+download_xml
+download_icon
+define_retronas
+echo ""
+echo ""
+echo ""
+echo "Done. Now goto the VMs tab and tou will see the RetroNAS VM installed. Start VM with console VNC "
+echo "Start VM with console VNC"
+echo "Login with defualt username 'retronas' and passwaord 'retronas'"
+echo "Once logged in, type retronas to start the config wizard "
 
